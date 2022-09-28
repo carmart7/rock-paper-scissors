7be39a47126e72f0e8698696e8bd12f5ebcb44ea
[carlos martinez] <theycallhellp@gmail.com>
2022-09-28 12:26:47 -0700

    add variables and win/loss/tie logic

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Rock Paper Scissors
 // 0. var for WinsLosses,Ties set to 0
+var wins = 0;
+var losses = 0;
+var ties = 0;
 // 1. Window Prompt (R, P, S)
 //      assign var humanChoice
+var humanChoice = prompt("What's your choice (R, P, S)?");
 // 2. Generate Input
 //      assign var computerChoice
+var choices = ['R', 'P', 'S'];
+var computerChoice = choices[computerNumber()];
+
 // 3. Compare humanChoice with computerChoice
 //      A) If humanChoice === computerChoice prompt Tie
             // 1a) Prompt tie increase Tie Score
@@ -24,7 +31,38 @@
 //      D)  If humanChoice = S
 //          1a computerChoice = P prompt Win
 //          2a computerChoice = R prompt Lose
+if (humanChoice == computerChoice) {
+    alert("That's a tie!");
+    ties = ties + 1;
+} else if (humanChoice == 'R'){
+    if (computerChoice == 'S') {
+        alert('You win!');
+        wins = wins + 1;
+    } else if (computerChoice == 'P') {
+        alert('You lose!');
+        losses = losses + 1;
+    }
+} else if (humanChoice == 'P'){
+    if (computerChoice == 'R') {
+        alert('You win!');
+        wins = wins + 1;
+    } else if (computerChoice == 'S') {
+        alert('You lose!');
+        losses = losses + 1;
+    }
+} else if (humanChoice == 'S'){
+    if (computerChoice == 'P') {
+        alert('You win!');
+        wins = wins + 1;
+    } else if (computerChoice == 'R') {
+        alert('You lose!');
+        losses = losses + 1;
+    }
+}
 // 4. Declare Wins, Losses, Ties
 // 5. Prompt Scorecard
 // 6. Prompt Play Again?
 
+function computerNumber() {
+    return Math.floor(Math.random()*3);
+}

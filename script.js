1e2d394bc36293ef60797bcf71d93e41b6fb767f
coderchrisdean <coderchrisdean@gmail.com>
2022-09-28 12:11:27 -0700

    PSEUDO CODE added to script

---
+++ script.js
@@ -0,0 +1,30 @@
+// Rock Paper Scissors
+// 0. var for WinsLosses,Ties set to 0
+// 1. Window Prompt (R, P, S)
+//      assign var humanChoice
+// 2. Generate Input
+//      assign var computerChoice
+// 3. Compare humanChoice with computerChoice
+//      A) If humanChoice === computerChoice prompt Tie
+            // 1a) Prompt tie increase Tie Score
+//      B) If humanChoice = R
+//          1a computerChoice = S prompt Win
+                // a) Add 1 Win for User
+//          2a computerChoice = P prompt Lose
+//                 -- Add 1 Loss for User
+
+//      C)  If humanChoice = P
+
+//          1a computerChoice = R prompt Win
+//                 -- Add 1 Win for User
+
+//          2a computerChoice = S prompt Lose//    
+//                 -- Add 1 Loss for User
+
+//      D)  If humanChoice = S
+//          1a computerChoice = P prompt Win
+//          2a computerChoice = R prompt Lose
+// 4. Declare Wins, Losses, Ties
+// 5. Prompt Scorecard
+// 6. Prompt Play Again?
+
